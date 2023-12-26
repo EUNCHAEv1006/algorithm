dff446521c26e8afadf793568d158db1b251171e
김은채 <ke808762@gmail.com>
2023-12-26 20:30:36 +0900

    배열의 평균값

---
+++ lv1/배열의_평균값/Solution.java
@@ -0,0 +1,18 @@
+package lv1.배열의_평균값;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(solution(numbers));
+    }
+
+    public static double solution(int[] numbers) {
+        double answer = 0;
+        int tot=0;
+        for(int i=0; i<numbers.length; i++) {
+            tot+=numbers[i];
+        }
+        answer=(double) tot/numbers.length;
+        return answer;
+    }
+}
