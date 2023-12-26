7b08fecb42bfc7f80a3f54f6048828265bd67b81
김은채 <ke808762@gmail.com>
2023-12-26 18:10:34 +0900

    두 수의 곱

---
+++ lv1/두_수의_곱/Solution.java
@@ -0,0 +1,14 @@
+package lv1.두_수의_곱;
+
+public class Solution {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 4;
+        System.out.println(solution(num1, num2));
+    }
+
+    public static int solution(int num1, int num2) {
+        int answer = 0;
+        return num1*num2;
+    }
+}
