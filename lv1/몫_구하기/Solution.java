bba8e9d722cf29112b0dae0bfda3aa8cba2d0c47
김은채 <ke808762@gmail.com>
2023-12-26 18:13:22 +0900

    몫 구하기

---
+++ lv1/몫_구하기/Solution.java
@@ -0,0 +1,14 @@
+package lv1.몫_구하기;
+
+public class Solution {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 4;
+        System.out.println(solution(num1, num2));
+    }
+
+    public static int solution(int num1, int num2) {
+        int answer = 0;
+        return num1/num2;
+    }
+}
