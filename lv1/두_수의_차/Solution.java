9c5bb7cfebb89f4ff8391bccb16240a2d6f91724
김은채 <ke808762@gmail.com>
2023-12-26 18:06:04 +0900

    두 수의 차

---
+++ lv1/두_수의_차/Solution.java
@@ -0,0 +1,14 @@
+package lv1.두_수의_차;
+
+public class Solution {
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+        System.out.println(solution(num1, num2));
+    }
+
+    public static int solution(int num1, int num2) {
+        int answer = 0;
+        return num1-num2;
+    }
+}
