bbf42f5672f6f1c57dfc2ff24f1b2b8f6da6bacf
김은채 <ke808762@gmail.com>
2023-12-26 20:47:37 +0900

    나머지가 1이 되는 수 찾기

---
+++ lv2/나머지가_1이_되는_수_찾기/Solution.java
@@ -0,0 +1,21 @@
+package lv2.나머지가_1이_되는_수_찾기;
+
+public class Solution {
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(solution(n));
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+
+        for(int x=1; x<n; x++){
+            if(n%x==1){
+                answer=answer+x;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
