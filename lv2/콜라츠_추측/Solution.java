e763a6f39a66a589beac79077557bd15dc64f923
김은채 <ke808762@gmail.com>
2023-12-28 10:08:31 +0900

    콜라츠 추측

---
+++ lv2/콜라츠_추측/Solution.java
@@ -0,0 +1,32 @@
+package lv2.콜라츠_추측;
+
+public class Solution {
+    public static void main(String[] args) {
+//        int num = 6;
+//        int num = 16;
+        int num = 626331;
+        System.out.println(solution(num));
+    }
+
+//    public static int solution(int num) { <- num=62631일 때, 오버플로우 발생하기 때문에 Long num으로 변경함
+    public static int solution(long num) {
+        int count = 0;
+
+
+        while (num != 1) {
+            if (num%2 == 0) {
+                num /= 2;
+            } else {
+                num = (num*3) + 1;
+            }
+            count++;
+
+            if (count > 500 && num != 1) {
+                return -1;
+            }
+        }
+
+
+        return count;
+    }
+}
