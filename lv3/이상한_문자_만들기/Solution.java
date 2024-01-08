409323e51b2c37a059d560d2c4c09c9f4f608d8c
김은채 <ke808762@gmail.com>
2024-01-09 00:36:39 +0900

    new: 세모이상한 문자 만들기

---
+++ lv3/이상한_문자_만들기/Solution.java
@@ -0,0 +1,32 @@
+package lv3.이상한_문자_만들기;
+
+import java.util.Locale;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "try hello world";
+        System.out.println(solution(s));
+    }
+
+    public static String solution(String s) {
+        String answer = "";
+
+        int cnt = 0;
+        String[] strArr = s.split("");
+
+        for (String string : strArr) {
+            if (string.contains(" ")) {
+                cnt = 0;
+            } else
+                cnt++;
+
+            if (cnt % 2 == 0) {
+                answer += string.toLowerCase();
+            } else {
+                answer += string.toUpperCase();
+            }
+        }
+
+        return answer;
+    }
+}
