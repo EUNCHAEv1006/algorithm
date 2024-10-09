324a6133583c876f1bbcf2d9d7f11d540aa6cc59
KIM EUN CHAE <129070298+EUNCHAEv1006@users.noreply.github.com>
2024-10-10 00:38:30 +0900

    [Silver II] Title: 스택 수열, Time: 684 ms, Memory: 100632 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -4,25 +4,23 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) {
         /*
-            N(수열 개수) A[](수열 배열)
-            수열 배열 채우기
-            for(N만큼 반복) {
-                if(현재 수열 값 >= 오름차순 자연수) {
-                    while(현재 수열값 == 오름차순 자연수 까지) {
-                        push()
-                        (+) 저장
-                    }
-                    pop()
-                    (-) 저장
-                    }
-                    else(현재 수열값 < 오름차순 자연수) {
-                        pop()
-                        if(스택 pop 결괏값 > 수열의 수) NO 출력
-                        else(-) 저장
-                    }
+        N(수열 개수) A[](수열 배열)
+        수열 배열 채우기
+        for(N만큼 반복){
+            if(현재 수열 값 >= 오름차순 자연수) {
+                while(값이 같아질 때까지) {
+                    push()
+                    (+) 저장
                 }
-                if(-값을 출력한 적이 없으면) 저장한 값 출력
+                pop()
+                (-) 저장
+            } else {
+                pop()
+                if(스택 pop 결괏값 > 수열의 수) NO 출력
+                else (-) 저장
             }
+        }
+        if(-값을 출력한 적이 없으면) 저장한 값 출력
          */
 
         Scanner scanner = new Scanner(System.in);
@@ -30,27 +28,28 @@ public class Main {
         int N = scanner.nextInt();
         int[] A = new int[N];
 
-        for (int i=0; i<N; i++) {
+        for(int i=0; i<N; i++){
             A[i] = scanner.nextInt();
         }
 
         Stack<Integer> stack = new Stack<>();
-        int num = 1; // 오름차순 자연수
+        int num = 1; // 오름차순으로 해야하기 때문
         boolean result = true;
         StringBuffer stringBuffer = new StringBuffer();
-        for(int i =0; i<N; i++) {
+
+        for(int i=0; i<N; i++){
             int su = A[i]; // 수열에 있는 값 가져오기
-            if (su >= num) {
-                while (su >= num) {
+            if(su >= num) {
+                while(su >= num){
                     stack.push(num++);
                     stringBuffer.append("+\n");
                 }
                 stack.pop();
                 stringBuffer.append("-\n");
             } else {
-                int n = stack.pop(); // data를 빼온다
+                int n = stack.pop();
                 if (n > su) {
-                    System.out.println("NO");
+                    System.out.print("NO");
                     result = false;
                     break;
                 } else {
@@ -58,6 +57,8 @@ public class Main {
                 }
             }
         }
-        if (result) System.out.println(stringBuffer.toString());
+        if (result) {
+            System.out.println(stringBuffer);
+        }
     }
 }
